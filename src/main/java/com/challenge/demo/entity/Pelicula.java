4fc0e849d5db1f8954fb4349feebdc571d1c4898
Damian Cejas <damianfcejas@gmail.com>
2022-08-18 19:55:30 -0300

    Creacion modelo pelicula y mejoramiento personaje

--- src/main/java/com/challenge/demo/entity/Pelicula.java
+++ src/main/java/com/challenge/demo/entity/Pelicula.java
@@ -1,36 +1,75 @@
 package com.challenge.demo.entity;
 
 import java.util.Date;
+import java.util.List;
 import javax.persistence.Basic;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 import org.springframework.format.annotation.DateTimeFormat;
 
 
 @Getter @Setter
-@Entity
+@ToString
+@Entity (name = "peliculas")
 public class Pelicula {
     
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "pel_id")    
     private Integer idpelicula;
     
     @Basic
+    @Column(name = "pel_imagen")    
     private String imagen;
+    
+    @NotNull(message = "El título es obligatorio")
+    @Column(name = "pel_titulo")    
     private String titulo;
+    
+    @NotNull(message = "La fecha de creación es obligatioria")
     @DateTimeFormat(pattern="yyyy-MM-dd")
+    @Column(name = "pel_fechaCreacion")    
     private Date fechaCreacion;
-    //@Min(value = 1,message = "La calificacion debe ser mayor que 1 y menor que 5")
-    //@Max(value = 5,message = "La calificacion debe ser mayor que 1 y menor que 5")
+    
+    @NotNull(message = "La calificación es obligatoria")
+    @Min(value = 1,message = "La calificacion admite valores del 1 al 5")
+    @Max(value = 5,message = "La calificacion admite valores del 1 al 5")
+    @Column(name = "pel_calificacion")    
     private Integer calificacion;
     
-    @OneToMany
-    private Personaje personaje;
+    @OneToMany (mappedBy = "personaje")
+    List <Personaje> personajeList;
+    
+    @ManyToOne
+    @JoinColumn (name = "pel_genero")
+    private Genero genero;
+
+    public Pelicula() {
+    }
+
+    public Pelicula(Integer idpelicula, String imagen, String titulo, Date fechaCreacion, Integer calificacion, List<Personaje> personajeList, Genero genero) {
+        this.idpelicula = idpelicula;
+        this.imagen = imagen;
+        this.titulo = titulo;
+        this.fechaCreacion = fechaCreacion;
+        this.calificacion = calificacion;
+        this.personajeList = personajeList;
+        this.genero = genero;
+    }
+    
+    
     
     
 }
--- src/main/java/com/challenge/demo/entity/Personaje.java
+++ src/main/java/com/challenge/demo/entity/Personaje.java
@@ -1,36 +1,57 @@
 package com.challenge.demo.entity;
 
 import javax.persistence.Basic;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 
-@Entity
+@Entity (name = "personajes")
 @Getter @Setter
+@ToString
 public class Personaje {
     
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "per_id")
     private Integer idpersonaje;
     
     @Basic
+    @Column(name = "per_imagen")
     private String imagen;
+
+    @Column(name = "per_nombre")
+    @NotNull(message = "El nombre es obligatorio")
     private String nombre;
-    private String edad;
+
+    @Column(name = "per_edad")
+    @NotNull(message = "La edad es obligatoria")
+    private Integer edad;
+    
+    @Column(name = "per_peso")
+    @NotNull(message = "El peso es obligatorio")
     private Integer peso;
+    
+    @Column(name = "per_historia")
+    @NotNull(message = "La historia es obligatoria")
     private String historia;
+    
     @ManyToOne
+    @JoinColumn(name = "per_pelicula")
     private Pelicula pelicula;
 
     public Personaje() {
     }
 
-    public Personaje(Integer idpersonaje, String imagen, String nombre, String edad, Integer peso, String historia, Pelicula pelicula) {
+    public Personaje(Integer idpersonaje, String imagen, String nombre, Integer edad, Integer peso, String historia, Pelicula pelicula) {
         this.idpersonaje = idpersonaje;
         this.imagen = imagen;
         this.nombre = nombre;
