ccf429795b5a13b399bbe22ae8db97b219d2e309
Damian Cejas <damianfcejas@gmail.com>
2022-08-18 22:37:35 -0300

    Creacion IPeliculaService

--- src/main/java/com/challenge/demo/Interface/IPeliculaService.java
+++ src/main/java/com/challenge/demo/Interface/IPeliculaService.java
@@ -1,5 +1,13 @@
 package com.challenge.demo.Interface;
 
+import com.challenge.demo.entity.Pelicula;
+import java.util.List;
+
 public interface IPeliculaService {
     
+    public List<Pelicula> obtenerPelicula();
+    public Pelicula crearPelicula (Pelicula pelicula);
+    public void borrarPelicula (Integer id);
+    public Pelicula obtenerPelicula (Integer id);
+    public void modificarPelicula (Pelicula pelicula);
 }
