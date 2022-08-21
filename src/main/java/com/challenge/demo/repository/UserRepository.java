526e4f932e3e165fd093e766de864ee92b8a0eba
Damian Cejas <damianfcejas@gmail.com>
2022-08-21 13:43:27 -0300

    Creacion pwd enc, añadiendo metodo a repo user

--- src/main/java/com/challenge/demo/repository/UserRepository.java
+++ src/main/java/com/challenge/demo/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package com.challenge.demo.repository;
 
 import com.challenge.demo.entity.User;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer>{
+    List<User> findByUsernameAndIsEnabledTrue(String username);
     
 }
