166ecf3204ad4b6e59e9a5d8aa12bd94152f2667
Damian Cejas <damianfcejas@gmail.com>
2022-08-21 14:00:38 -0300

    Creacion servicio user

--- src/main/java/com/challenge/demo/service/UserService.java
+++ src/main/java/com/challenge/demo/service/UserService.java
@@ -1,13 +1,43 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.challenge.demo.service;
 
-/**
- *
- * @author ubuser
- */
+import com.challenge.demo.entity.User;
+import com.challenge.demo.repository.UserRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+    
+    @Autowired
+    UserRepository repository;
+    
+    @Autowired
+    PasswordEncoder passwordEncoder;
+    
+    public boolean isUserEnabled(User user) {
+        boolean isUserEnabled = false;
+        List<User> usuarios = repository.findByUsernameAndIsEnabledTrue(user.getEmail());
+        if (!usuarios.isEmpty()) {
+            User usuario = usuarios.get(0);
+            if (passwordEncoder.matches(user.getPassword(), usuario.getPassword()))
+                isUserEnabled = true;
+        }
+        
+        return isUserEnabled;
+    }
+    
+    public void crearUsuario(User usuario) throws Exception {
+        List<User> usuarios = repository.findByUsernameAndIsEnabledTrue(usuario.getEmail());
+        if(!usuarios.isEmpty()){
+            throw new Exception("El mail ya está registrado!");
+        } else {
+        
+        usuario.setPassword(passwordEncoder.encode(usuario.getPassword()));
+        repository.save(usuario);
+        }
+    
+    }
     
 }
